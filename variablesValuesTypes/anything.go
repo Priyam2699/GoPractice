9a4bf8827ebe19f865e7d8c114b117b9bfcbab84
Priyam Dua <duap@uwindsor.ca>
2022-10-30 16:16:52 -0400

    creates own type

--- variablesValuesTypes/anything.go
+++ variablesValuesTypes/anything.go
@@ -2,27 +2,19 @@ package main
 
 import "fmt"
 
-var y = 911
-var z = "Shaken not stirred"
-var x = true
+var a int
 
-// %T used to check the type of the variable
-// is a static programming language
+// created my own type hotdog
+type hotdog int
+
+var b hotdog
+
 func main() {
 
-	fmt.Println(y)
-	// %T gives type of the variable
-	fmt.Printf("%T \n", y)
-	// %b gives the binary value
-	fmt.Printf("%b \n", y)
-	fmt.Printf("%x \n", y)
-	fmt.Printf("%#x \n", y)
-
-	fmt.Printf("%T \n", z)
-	fmt.Printf("%T \n", x)
-
-	str := fmt.Sprintf("%#x \n", y)
-	fmt.Println(len(str))
-	fmt.Printf("%v", z) // %v gives real value
-
+	a = 42
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+	b = 21
+	fmt.Println(b)
+	fmt.Printf("%T\n", b)
 }
