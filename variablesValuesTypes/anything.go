a5484ff9634c08b5c98ed52b31d1bf8ee8a6fce0
Priyam Dua <duap@uwindsor.ca>
2022-10-30 16:11:32 -0400

    uses fmt package

--- variablesValuesTypes/anything.go
+++ variablesValuesTypes/anything.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-var y = 21
+var y = 911
 var z = "Shaken not stirred"
 var x = true
 
@@ -11,8 +11,18 @@ var x = true
 func main() {
 
 	fmt.Println(y)
+	// %T gives type of the variable
 	fmt.Printf("%T \n", y)
+	// %b gives the binary value
+	fmt.Printf("%b \n", y)
+	fmt.Printf("%x \n", y)
+	fmt.Printf("%#x \n", y)
+
 	fmt.Printf("%T \n", z)
 	fmt.Printf("%T \n", x)
+
+	str := fmt.Sprintf("%#x \n", y)
+	fmt.Println(len(str))
+	fmt.Printf("%v", z) // %v gives real value
 
 }
