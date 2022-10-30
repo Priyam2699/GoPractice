07604cd787610289f93c8ba93c37e8386777b700
Priyam Dua <duap@uwindsor.ca>
2022-10-30 17:18:37 -0400

    uses numeric types

--- prgrammingFundamentals/org.go
+++ prgrammingFundamentals/org.go
@@ -2,14 +2,15 @@ package main
 
 import "fmt"
 
-var x bool
+var a int32
+var b float32
+var c int8 = 555 // as the range of int8 is -127 to 128
 
 func main() {
-	a := 7
-	b := 14
-	fmt.Println(x)
-	x = true
-	fmt.Println(x)
-	fmt.Println(a == b)
-	fmt.Println(a != b)
+	x := 42
+	y := 42.1521
+	fmt.Printf("%T \n", x)
+	fmt.Printf("%T \n", y)
+	fmt.Printf("%T \n", a)
+	fmt.Printf("%T \n", b)
 }
