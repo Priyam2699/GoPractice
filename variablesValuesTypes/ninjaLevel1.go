176675e87def2461e926c5fc44e20c4add095011
Priyam Dua <duap@uwindsor.ca>
2022-10-30 16:32:55 -0400

    hands on 3

--- variablesValuesTypes/ninjaLevel1.go
+++ variablesValuesTypes/ninjaLevel1.go
@@ -8,8 +8,13 @@ var z bool
 
 func main() {
 
+	x = 42
+	y = "James Bond"
+	z = true
 	fmt.Printf("%v %T\n", x, x)
 	fmt.Printf("%v %T\n", y, y)
 	fmt.Printf("%v %T\n", z, z)
+	s := fmt.Sprintf("%v %v %v", x, y, z)
+	fmt.Println(s)
 
 }
