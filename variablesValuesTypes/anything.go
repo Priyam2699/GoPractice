b0c21090c49bb7f72c41344ce7add7ac063b5e4c
Priyam Dua <duap@uwindsor.ca>
2022-10-30 02:04:24 -0400

    uses short declaration operator

--- variablesValuesTypes/anything.go
+++ variablesValuesTypes/anything.go
@@ -3,19 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Priyam, How are you?")
 
-	foo()
-
-	i := 0
-	for i = 0; i < 10; i++ {
-		fmt.Println(i)
-	}
-
-	fmt.Printf("End")
-}
-
-func foo() {
-
-	fmt.Println("I am in foo")
+	x := 42
+	fmt.Println(x)
+	x = 100
+	fmt.Println(x)
 }
