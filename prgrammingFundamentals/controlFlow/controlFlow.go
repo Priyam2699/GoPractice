c9f974c23a9e5a2e0d91e6783e1403e412cfa653
Priyam Dua <duap@uwindsor.ca>
2022-10-31 16:31:23 -0400

    if else and modulas

--- prgrammingFundamentals/controlFlow/controlFlow.go
+++ prgrammingFundamentals/controlFlow/controlFlow.go
@@ -4,15 +4,9 @@ import "fmt"
 
 func main() {
 
-	var x int
-	fmt.Scan(&x)
-
-	if x%2 == 0 {
-		fmt.Println("Even")
-	} else if x < 0 {
-		fmt.Println("Number if less than 0")
-	} else {
-		fmt.Println("Number is odd")
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
 	}
-
 }
