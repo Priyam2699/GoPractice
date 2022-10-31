0c73d2a7c2a27cd91eba480d5876f1d6d85a5101
Priyam Dua <duap@uwindsor.ca>
2022-10-31 14:48:52 -0400

    nested loops

--- prgrammingFundamentals/controlFlow/controlFlow.go
+++ prgrammingFundamentals/controlFlow/controlFlow.go
@@ -4,9 +4,14 @@ import "fmt"
 
 func main() {
 
-	for i := 1; i <= 10; i++ {
+	for i := 1; i <= 3; i++ {
 
-		fmt.Println(i)
+		fmt.Printf("Outer loop number %d\n", i)
+		for j := 1; j <= 3; j++ {
+			fmt.Printf("\tInner loop %d\n", j)
+
+		}
+		//fmt.Println(i)
 	}
 
 }
