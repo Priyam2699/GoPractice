6829ca6ba5748f3fef929e42c0868a10095fba97
Priyam Dua <duap@uwindsor.ca>
2022-10-31 16:54:12 -0400

    conditional logical operators

--- prgrammingFundamentals/controlFlow/controlFlow.go
+++ prgrammingFundamentals/controlFlow/controlFlow.go
@@ -6,35 +6,10 @@ func main() {
 
 	var x int
 	fmt.Scan(&x)
-	switch {
-	case x == 1:
-		fmt.Println("Number 1")
-	case x == 2:
-		fmt.Println("Number 2")
-		fallthrough
-	case x == 3:
-		fmt.Println("Number 3")
-		fallthrough
-	case x == 4:
-		fmt.Println("Number 4")
-		fallthrough
-	case x%2 == 0:
-		fmt.Println("Even number")
-	default:
-		fmt.Println("this is default")
 
-	}
-
-	var str string
-	fmt.Scan(&str)
-
-	switch str {
-	case "Priyam", "Dua":
-		fmt.Println("Priyam")
-	case "James":
-		fmt.Println("James")
-	default:
-		fmt.Println("Name does not match")
-
+	if x%2 == 0 && x%3 == 0 {
+		fmt.Println("Number divided by 2 and 3")
+	} else {
+		fmt.Println("Number not divided by 2 and 3")
 	}
 }
