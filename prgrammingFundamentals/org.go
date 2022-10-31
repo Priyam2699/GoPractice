0b5d84320e2684d0b26ea25eaf9cd1f242bd30ab
Priyam Dua <duap@uwindsor.ca>
2022-10-30 23:24:57 -0400

    uses iota keyword to set incremenrtal values from 0

--- prgrammingFundamentals/org.go
+++ prgrammingFundamentals/org.go
@@ -2,19 +2,30 @@ package main
 
 import "fmt"
 
-const a int64 = 42
-const b float32 = 42.555
-const c string = "James Bond"
-
-// or you can write this
+const (
+	a = iota
+	b
+	c
+)
 
 const (
-	d int32   = 41
-	e float64 = 41.555
-	f string  = "Indiana Jones"
+	d = iota
+	e
+	f
+	g
 )
 
 func main() {
 
-	fmt.Println(a, b, c, d, e, f)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
+
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+	fmt.Printf("%T\n", c)
 }
