31b7a83168c6d473c3f9aa8955aa3192370afd72
Priyam Dua <duap@uwindsor.ca>
2022-10-31 13:26:17 -0400

    hands on  4 bitwise operators

--- prgrammingFundamentals/ninjaLevel2.go
+++ prgrammingFundamentals/ninjaLevel2.go
@@ -2,17 +2,10 @@ package main
 
 import "fmt"
 
-const (
-	a     = 42
-	b int = 41
-)
-
 func main() {
 
-	a := 42 == 42
-	b := 42 >= 41
-	c := 42 <= 41
-	d := 42 != 41
-	fmt.Println(a, b, c, d)
-
+	s := 25
+	fmt.Printf("%d\t%b\t%#x\n", s, s, s)
+	t := s << 1
+	fmt.Printf("%d\t%b\t%#x", t, t, t)
 }
