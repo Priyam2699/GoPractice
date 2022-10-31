8a7155a00a1bab65f0bbfa755dce80473721d165
Priyam Dua <duap@uwindsor.ca>
2022-10-31 13:32:56 -0400

    hands on  5 raw string literal

--- prgrammingFundamentals/ninjaLevel2.go
+++ prgrammingFundamentals/ninjaLevel2.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main() {
 
-	s := 25
-	fmt.Printf("%d\t%b\t%#x\n", s, s, s)
-	t := s << 1
-	fmt.Printf("%d\t%b\t%#x", t, t, t)
+	// uses a raw string literal
+	s := `Here is a Raw
+    String
+                Literal
+`
+
+	fmt.Println(s)
 }
