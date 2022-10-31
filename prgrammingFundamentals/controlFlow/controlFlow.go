d568c351e6d7f8caaa64db073a10a13422ad7a20
Priyam Dua <duap@uwindsor.ca>
2022-10-31 16:46:51 -0400

    switch case fallthrough

--- prgrammingFundamentals/controlFlow/controlFlow.go
+++ prgrammingFundamentals/controlFlow/controlFlow.go
@@ -4,9 +4,37 @@ import "fmt"
 
 func main() {
 
-	for i := 0; i < 100; i++ {
-		if i%2 == 0 {
-			fmt.Println(i)
-		}
+	var x int
+	fmt.Scan(&x)
+	switch {
+	case x == 1:
+		fmt.Println("Number 1")
+	case x == 2:
+		fmt.Println("Number 2")
+		fallthrough
+	case x == 3:
+		fmt.Println("Number 3")
+		fallthrough
+	case x == 4:
+		fmt.Println("Number 4")
+		fallthrough
+	case x%2 == 0:
+		fmt.Println("Even number")
+	default:
+		fmt.Println("this is default")
+
+	}
+
+	var str string
+	fmt.Scan(&str)
+
+	switch str {
+	case "Priyam", "Dua":
+		fmt.Println("Priyam")
+	case "James":
+		fmt.Println("James")
+	default:
+		fmt.Println("Name does not match")
+
 	}
 }
