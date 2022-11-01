be5c86c4a673019984474779f556f65e7af49beb
Priyam Dua <duap@uwindsor.ca>
2022-11-01 16:18:45 -0400

    multidimensional slice with range

--- prgrammingFundamentals/GroupingData/ninjaLevel4.go
+++ prgrammingFundamentals/GroupingData/ninjaLevel4.go
@@ -4,19 +4,25 @@ import "fmt"
 
 func main() {
 
-	states := make([]string, 50, 50)
+	x := []string{"James", "Bond", "007"}
+	y := []string{"Money", "Penny", "009"}
 
-	states = append(states, ` Alabama`, ` Alaska`, ` Arizona`, ` Arkansas`, ` California`, ` Colorado`, ` Connecticut`, ` Delaware`, ` Florida`, ` Georgia`, ` Hawaii`, ` Idaho`, ` Illinois`, ` Indiana`, ` Iowa`, ` Kansas`, ` Kentucky`, ` Louisiana`, ` Maine`, ` Maryland`, ` Massachusetts`, ` Michigan`, ` Minnesota`, ` Mississippi`, ` Missouri`, ` Montana`, ` Nebraska`, ` Nevada`, ` New Hampshire`, ` New Jersey`, ` New Mexico`, ` New York`, ` North Carolina`, ` North Dakota`, ` Ohio`, ` Oklahoma`, ` Oregon`, ` Pennsylvania`, ` Rhode Island`, ` South Carolina`, ` South Dakota`, ` Tennessee`, ` Texas`, ` Utah`, ` Vermont`, ` Virginia`, ` Washington`, ` West Virginia`, ` Wisconsin`, ` Wyoming`)
+	fmt.Println(x)
+	fmt.Println(y)
 
-	states = append(states[0:0], states[50:]...)
-	fmt.Println(states)
-	fmt.Printf("%T\n", states)
-	fmt.Println("Length of Slice", len(states))
-	fmt.Println("Capacity of Slice", cap(states))
+	multi := [][]string{x, y}
 
-	for i := 0; i < len(states); i++ {
+	fmt.Println(multi)
 
-		fmt.Println(i, states[i])
+	// i is index and v is slices
+	for i, v := range multi {
+
+		fmt.Println("Record", i)
+
+		for j, t := range v {
+			fmt.Printf("\t Index Position: %v\tValue: %v\n", j, t)
+		}
+		fmt.Println()
 
 	}
 
