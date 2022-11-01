fdca86b0e7c2a62d2d6f4f42cdaad633c47e8f58
Priyam Dua <duap@uwindsor.ca>
2022-10-31 20:11:03 -0400

    hands on 2 rune code

--- prgrammingFundamentals/controlFlow/ninjaLevel3.go
+++ prgrammingFundamentals/controlFlow/ninjaLevel3.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 
-	for i := 0; i <= 10000; i++ {
+	for i := 65; i <= 90; i++ {
+
 		fmt.Println(i)
+		for j := 0; j < 3; j++ {
+
+			fmt.Printf("%#U\n", i)
+		}
 	}
 
 }
