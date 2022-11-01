c06541d1c3fba201decc4641fb3627ce403b74e8
Priyam Dua <duap@uwindsor.ca>
2022-11-01 15:30:23 -0400

    delete from slice usign append and slicing

--- prgrammingFundamentals/GroupingData/ninjaLevel4.go
+++ prgrammingFundamentals/GroupingData/ninjaLevel4.go
@@ -4,17 +4,9 @@ import "fmt"
 
 func main() {
 
-	x := []int{42, 43, 44, 45, 46, 47, 48, 79, 50, 51}
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
 
-	fmt.Println(x[:5])
-	fmt.Println(x[5:])
-	fmt.Println(x[2:7])
-	fmt.Println(x[1:6])
-
-	x = append(x, 52)
-	fmt.Println(x)
-
-	x = append(x, []int{56, 57, 58, 59, 60}...)
+	x = append(x[:3], x[6:]...)
 
 	fmt.Println(x)
 
