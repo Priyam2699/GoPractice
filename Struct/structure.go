a53270f523a7208bb6acda06162713590706a7a1
Priyam Dua <duap@uwindsor.ca>
2022-11-01 18:46:47 -0400

    anonymous struct

--- Struct/structure.go
+++ Struct/structure.go
@@ -9,40 +9,25 @@ type person struct {
 	age   int
 }
 
-type secretAgent struct {
-	person
-
-	LTK bool
-}
-
 func main() {
 
-	// p1 of type person
 	p1 := person{
 
 		first: "Priyam",
 		last:  "Dua",
-		age:   29,
+		age:   24,
 	}
 
-	p2 := person{
+	// anonymous struct
+	x := struct {
+		email        string
+		mobileNumber int
+	}{
 
-		first: "Ajay",
-		last:  "Maheshwary",
+		email:        "priyamdua26@gmail.com",
+		mobileNumber: 5195621651,
 	}
 
-	fmt.Println(p1.age, p2.age)
-
-	sa1 := secretAgent{
-		person: person{
-			first: "James",
-			last:  "Bond",
-			age:   32,
-		},
-
-		LTK: true,
-	}
-
-	fmt.Println(sa1, sa1.person, sa1.person.first, sa1.last, sa1.age, sa1.LTK)
-
+	fmt.Println(p1)
+	fmt.Println(x)
 }
