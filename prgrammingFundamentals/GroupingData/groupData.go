5923477148e6bf8d0f1b5763073f4e9a505237e1
Priyam Dua <duap@uwindsor.ca>
2022-10-31 21:43:02 -0400

    appending to a slice

--- prgrammingFundamentals/GroupingData/groupData.go
+++ prgrammingFundamentals/GroupingData/groupData.go
@@ -7,11 +7,19 @@ func main() {
 	x := []int{4, 5, 7, 8, 42}
 	fmt.Println(len(x))
 	fmt.Println(x)
-	fmt.Println(x[1:])  // start from position 1
-	fmt.Println(x[1:4]) // start from 1 and end at position 4
-	fmt.Println(x[:4])  // end at position 4
 
-	for i := 0; i <= 4; i++ {
-		fmt.Println(i, x[i])
-	}
+	// appends element at end of slice
+
+	x = append(x, 85, 23, 76)
+
+	fmt.Println(len(x))
+	fmt.Println(x)
+
+	y := []int{84, 36, 14, 97}
+
+	// appending a slice to other slice
+	x = append(x, y...)
+	fmt.Println(len(x))
+	fmt.Println(x)
+
 }
