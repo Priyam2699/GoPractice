4f4fc4fa9bff728e760fe4638efc3490bddce8b3
Priyam Dua <duap@uwindsor.ca>
2022-11-01 18:29:17 -0400

    embedded structs

--- Struct/structure.go
+++ Struct/structure.go
@@ -9,6 +9,12 @@ type person struct {
 	age   int
 }
 
+type secretAgent struct {
+	person
+
+	LTK bool
+}
+
 func main() {
 
 	// p1 of type person
@@ -26,5 +32,17 @@ func main() {
 	}
 
 	fmt.Println(p1.age, p2.age)
+
+	sa1 := secretAgent{
+		person: person{
+			first: "James",
+			last:  "Bond",
+			age:   32,
+		},
+
+		LTK: true,
+	}
+
+	fmt.Println(sa1, sa1.person, sa1.person.first, sa1.last, sa1.age, sa1.LTK)
 
 }
